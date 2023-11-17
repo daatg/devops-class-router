a6e3c7ade8d5265acd68e9457b5b886366a2ae77
hdschnei <hdschnei@ncsu.edu>
2023-11-16 19:33:36 -0500

    feat(router): refactors and timeout fixes

--- router/src/main.rs
+++ router/src/main.rs
@@ -13,6 +13,7 @@ use std::hash::Hasher;
 use std::net::ToSocketAddrs;
 use std::sync::RwLock;
 use std::time::{Duration, Instant};
+use tokio::time::timeout;
 use url::Url;
 
 /// Router / load balancer
@@ -26,38 +27,63 @@ enum RoutingStrategy {
 }
 
 const AVERAGE_WINDOW: Duration = Duration::from_secs(2);
+const GRACE_REQUESTS: usize = 1;
+const TIMEOUT_THRESHHOLD: Duration = Duration::from_millis(500);
+const TIMEOUT_PENALTY: Duration = Duration::from_millis(500);
 
-struct ResponseTime {
+struct ResponseInfo {
     start: Instant,
-    wait: Duration,
+    wait: Option<Duration>,
 }
 
-fn pick_host_from_hash<T: Hash, V>(url: &Vec<V>, key: T) -> usize {
+fn pick_host_from_hash<T: Hash, V>(urls: &Vec<V>, key: T, exclude_indexes: &Vec<usize>) -> usize {
+    // Uses simple linear probing
+    fn probe(i: usize) -> usize {
+        i + 1
+    }
+    assert!(
+        urls.len() > exclude_indexes.len(),
+        "Cannot exclude all urls from a host pick!"
+    );
     let mut s = DefaultHasher::new();
     key.hash(&mut s);
-    (s.finish() % (url.len() as u64)) as usize
+    let mut out = (s.finish() % (urls.len() as u64)) as usize;
+    while exclude_indexes.contains(&out) {
+        out = probe(out) % urls.len();
+    }
+    out
 }
 
-fn pick_host_lowest_average(averages: &Vec<RwLock<Duration>>) -> usize {
+fn pick_host_lowest_average(
+    averages: &Vec<RwLock<Duration>>,
+    exclude_indexes: &Vec<usize>,
+) -> usize {
+    // Uses simple linear probing
+    fn probe(i: usize) -> usize {
+        i + 1
+    }
     let mut lowest = Duration::MAX;
-    let mut index = usize::MAX;
+    let mut out = usize::MAX;
     for i in 0..averages.len() {
         {
             // Acquire read lock
             let average = averages.get(i).unwrap().read().unwrap();
             if *average < lowest {
                 lowest = *average;
-                index = i;
+                out = i;
             }
             // Implicit drop
         }
     }
-    if index == usize::MAX {
+    if out == usize::MAX {
         let mut s = DefaultHasher::new();
         Instant::now().hash(&mut s);
-        return (s.finish() % (averages.len() as u64)) as usize;
+        out = (s.finish() % (averages.len() as u64)) as usize;
     }
-    index
+    while exclude_indexes.contains(&out) {
+        out = probe(out) % averages.len();
+    }
+    out
 }
 
 /// Forwards the incoming HTTP request using `awc` (actix web client).
@@ -68,51 +94,49 @@ async fn forward(
     client: web::Data<Client>,
     hosts: web::Data<Vec<Url>>,
     pool_count: web::Data<RwLock<isize>>,
-    times: web::Data<Vec<RwLock<VecDeque<ResponseTime>>>>,
+    times: web::Data<Vec<RwLock<VecDeque<ResponseInfo>>>>,
     averages: web::Data<Vec<RwLock<Duration>>>,
     agent_requests: web::Data<RwLock<HashMap<String, RwLock<VecDeque<Instant>>>>>,
 ) -> Result<HttpResponse, Error> {
     // TODO: parse the agent from the request body/headers
-    let agent = "Test123";
-    info!("Agent {agent} requested a host...");
+    let agent = "Test 123";
+    let routing_strategy = pick_strategy(agent, &agent_requests, &pool_count, &hosts);
+    let excludes: Vec<usize> = Vec::new();
 
-    let mut routing_strategy = RoutingStrategy::MovingAverage;
+    forward_request(
+        routing_strategy,
+        &excludes,
+        hosts,
+        agent,
+        averages,
+        req,
+        client,
+        peer_addr,
+        payload,
+        times,
+        pool_count,
+        agent_requests,
+    )
+    .await
+}
 
-    // Get read lock, we transform it into a write lock if needed
-    let agent_map = (**agent_requests).read().unwrap();
-    // New agent condition, make into write lock to add vecdeque
-    if agent_map.get(agent).is_none() {
-        info!("    Agent info was not found...");
-        drop(agent_map);
-        let mut agent_map = (**agent_requests).write().unwrap();
-        agent_map.insert(String::from(agent), RwLock::new(VecDeque::new()));
-        // Implicit drop of `agent_map`
-    } else {
-        let agent_times = agent_map.get(agent).unwrap().read().unwrap();
-        info!(
-            "    Agent info was found with {} requests in the window...",
-            agent_times.len()
-        );
-        let agent_total = agent_times.len();
-        drop(agent_times);
-        drop(agent_map);
-        // Acquire lock
-        let pool = (**pool_count).read().unwrap();
-        let traffic_total = *pool;
-        drop(pool);
-        // Calculate traffic share:
-        let threshhold = (traffic_total as f32) / ((**hosts).len() as f32);
-        info!("    Traffic threshhold is {}...", threshhold);
-
-        if agent_total as f32 > threshhold {
-            warn!("    Swapping {agent} to hash strategy...");
-            routing_strategy = RoutingStrategy::Hash;
-        }
-    }
-
+async fn forward_request(
+    routing_strategy: RoutingStrategy,
+    exclude_hosts: &Vec<usize>,
+    hosts: web::Data<Vec<Url>>,
+    agent: &str,
+    averages: web::Data<Vec<RwLock<Duration>>>,
+    req: HttpRequest,
+    client: web::Data<Client>,
+    peer_addr: Option<PeerAddr>,
+    payload: web::Payload,
+    times: web::Data<Vec<RwLock<VecDeque<ResponseInfo>>>>,
+    pool_count: web::Data<RwLock<isize>>,
+    agent_requests: web::Data<RwLock<HashMap<String, RwLock<VecDeque<Instant>>>>>,
+) -> Result<HttpResponse, Error> {
     let target_index = match routing_strategy {
-        RoutingStrategy::Hash => pick_host_from_hash(&(**hosts), agent),
-        RoutingStrategy::MovingAverage => pick_host_lowest_average(&(**averages)),
+        RoutingStrategy::Hash => pick_host_from_hash(&(**hosts), agent, exclude_hosts),
+        RoutingStrategy::MovingAverage => pick_host_lowest_average(&(**averages), exclude_hosts),
     };
 
     let mut target_url = (**hosts).get(target_index).unwrap().clone();
@@ -135,29 +159,61 @@ async fn forward(
 
     let request_start = Instant::now();
 
-    // match tokio::time::timeout(Duration::from_millis(10), work).await {
-    //     Ok(result) => match result {
-    //         Ok(response) => println!("Status: {}", response.status()),
-    //         Err(e) => println!("Network error: {:?}", e),
-    //     },
-    //     Err(_) => println!("Timeout: no response in 10 milliseconds."),
-    // };
+    let req = forwarded_req.send_stream(payload);
 
-    let res = forwarded_req
-        .send_stream(payload)
-        .await
-        .map_err(error::ErrorInternalServerError)?;
-
-    let request_time = ResponseTime {
-        start: request_start,
-        wait: request_start.elapsed(),
+    let res = match timeout(TIMEOUT_THRESHHOLD, req).await {
+        Ok(result) => result.map_err(error::ErrorInternalServerError),
+        Err(_) => Err(error::ErrorGatewayTimeout(
+            "Failed to reach host before timeout",
+        )),
     };
-    info!(
-        "    Reached target in: {}ms...",
-        request_time.wait.as_millis()
+
+    let request_time = ResponseInfo {
+        start: request_start,
+        wait: Some(request_start.elapsed()),
+    };
+    match request_time.wait {
+        Some(v) => info!("    Reached target in: {}ms...", v.as_millis()),
+        None => warn!(
+            "    Failed to reach target! Timeout at: {}ms...",
+            TIMEOUT_THRESHHOLD.as_millis()
+        ),
+    };
+
+    update_load_state(
+        times,
+        target_index,
+        request_time,
+        request_start,
+        averages,
+        pool_count,
+        agent_requests,
+        agent,
     );
 
-    let mut net: isize = 1; // Start at 1 for first add
+    let res = res?;
+    let mut client_resp = HttpResponse::build(res.status());
+    // Remove `Connection` as per
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Connection#Directives
+    for (header_name, header_value) in res.headers().iter().filter(|(h, _)| *h != "connection") {
+        client_resp.insert_header((header_name.clone(), header_value.clone()));
+    }
+
+    Ok(client_resp.streaming(res))
+}
+
+fn update_load_state(
+    times: web::Data<Vec<RwLock<VecDeque<ResponseInfo>>>>,
+    target_index: usize,
+    request_time: ResponseInfo,
+    request_start: Instant,
+    averages: web::Data<Vec<RwLock<Duration>>>,
+    pool_count: web::Data<RwLock<isize>>,
+    agent_requests: web::Data<RwLock<HashMap<String, RwLock<VecDeque<Instant>>>>>,
+    agent: &str,
+) {
+    let mut net: isize = 1;
+    // Start at 1 for first add
     let mut new_average: Duration = Duration::ZERO;
     {
         // Acquire lock
@@ -165,21 +221,20 @@ async fn forward(
         // Add new time
         host_times.push_back(request_time);
         // Drop old times
-        for i in 0..host_times.len() {
-            if host_times
-                .get(i)
-                .unwrap()
-                .start
-                .duration_since(request_start)
-                > AVERAGE_WINDOW
-            {
+        let mut i = 0;
+        while i < host_times.len() {
+            if request_start.duration_since(host_times.get(i).unwrap().start) < AVERAGE_WINDOW {
                 break;
             }
             host_times.pop_front();
             net -= 1;
+            i += 1;
         }
         for i in 0..host_times.len() {
-            new_average += host_times.get(i).unwrap().wait;
+            new_average += match host_times.get(i).unwrap().wait {
+                Some(v) => v,
+                None => TIMEOUT_PENALTY,
+            };
         }
         new_average = match host_times.len() == 0 {
             true => Duration::ZERO,
@@ -199,41 +254,75 @@ async fn forward(
         *count += net;
         // Implicit drop
     }
-    info!("    hI?...");
     {
         // Acquire lock
         let agent_map = (**agent_requests).read().unwrap();
         let mut agent_times = agent_map.get(agent).unwrap().write().unwrap();
         // Drop old times
         info!("    Remapping agent history...");
-        for i in 0..agent_times.len() {
+        let mut i = 0;
+        while i < agent_times.len() {
             info!(
                 "        {}ms request...",
-                agent_times
-                    .get(i)
-                    .unwrap()
-                    .duration_since(request_start)
+                request_start
+                    .duration_since(*agent_times.get(i).unwrap())
                     .as_millis()
             );
-            if agent_times.get(i).unwrap().duration_since(request_start) > AVERAGE_WINDOW {
-                info!("        Over {} -- dropped!", AVERAGE_WINDOW.as_millis());
+            if request_start.duration_since(*agent_times.get(i).unwrap()) < AVERAGE_WINDOW {
                 break;
             }
+            info!("        Over {} -- dropped!", AVERAGE_WINDOW.as_millis());
             agent_times.pop_front();
+            i += 1;
         }
         // Add new time
         agent_times.push_back(request_start);
         // Implicit drop
     }
+}
 
-    let mut client_resp = HttpResponse::build(res.status());
-    // Remove `Connection` as per
-    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Connection#Directives
-    for (header_name, header_value) in res.headers().iter().filter(|(h, _)| *h != "connection") {
-        client_resp.insert_header((header_name.clone(), header_value.clone()));
+fn pick_strategy(
+    agent: &str,
+    agent_requests: &web::Data<RwLock<HashMap<String, RwLock<VecDeque<Instant>>>>>,
+    pool_count: &web::Data<RwLock<isize>>,
+    hosts: &web::Data<Vec<Url>>,
+) -> RoutingStrategy {
+    info!("Agent {agent} requested a host...");
+
+    let mut routing_strategy = RoutingStrategy::MovingAverage;
+
+    // Get read lock, we transform it into a write lock if needed
+    let agent_map = (***agent_requests).read().unwrap();
+    // New agent condition, make into write lock to add vecdeque
+    if agent_map.get(agent).is_none() {
+        info!("    Agent info was not found...");
+        drop(agent_map);
+        let mut agent_map = (***agent_requests).write().unwrap();
+        agent_map.insert(String::from(agent), RwLock::new(VecDeque::new()));
+        // Implicit drop of `agent_map`
+    } else {
+        let agent_times = agent_map.get(agent).unwrap().read().unwrap();
+        info!(
+            "    Agent info was found with {} requests in the window...",
+            agent_times.len()
+        );
+        let agent_total = agent_times.len() - GRACE_REQUESTS;
+        drop(agent_times);
+        drop(agent_map);
+        // Acquire lock
+        let pool = (***pool_count).read().unwrap();
+        let traffic_total = *pool;
+        drop(pool);
+        // Calculate traffic share:
+        let threshhold = (traffic_total as f32) / ((***hosts).len() as f32);
+        info!("    Traffic threshhold is {}...", threshhold);
+
+        if agent_total as f32 > threshhold {
+            warn!("    Swapping {agent} to hash strategy...");
+            routing_strategy = RoutingStrategy::Hash;
+        }
     }
-
-    Ok(client_resp.streaming(res))
+    routing_strategy
 }
 
 #[actix_web::main]
@@ -256,7 +345,7 @@ async fn main() -> std::io::Result<()> {
             get_host("127.0.0.1", 3002),
         ];
         let pool_count = RwLock::new(0 as isize);
-        let mut times: Vec<RwLock<VecDeque<ResponseTime>>> = Vec::new();
+        let mut times: Vec<RwLock<VecDeque<ResponseInfo>>> = Vec::new();
         let mut averages: Vec<RwLock<Duration>> = Vec::new();
         for _ in 0..hosts.len() {
             times.push(RwLock::new(VecDeque::new()));
