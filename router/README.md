3c50a7936eb615b530bdc2b33ebe35999fc5cbeb
hdschnei <hdschnei@ncsu.edu>
2023-12-03 15:10:04 -0500

    misc(project): added more apache license clarity

---
+++ router/README.md
@@ -0,0 +1,4 @@
+Router / load balancer
+Built off of https://github.com/actix/examples/tree/master/http-proxy (Licensed under APACHE 2.0)
+Example code was pruned and then additional functionality added for
+load balancing, static/dynamic routing, etc.
--- router/src/main.rs
+++ router/src/main.rs
@@ -18,7 +18,7 @@ use tokio::time::timeout;
 use url::Url;
 
 /// Router / load balancer
-/// Built off of https://github.com/actix/examples/tree/master/http-proxy
+/// Built off of https://github.com/actix/examples/tree/master/http-proxy (Licensed under APACHE 2.0)
 /// Example code was pruned and then additional functionality added for
 /// load balancing, static/dynamic routing, etc.
 
